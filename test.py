0671a8b0fa0c6c643da37a284e9125cef1d4118f
xlmriosx <xlmriosx@gmail.com>
2024-07-08 16:24:59 -0300

    initial commit

---
+++ test.py
@@ -0,0 +1,7 @@
+anio_actual = 2024
+anio_nac= int(input('Ingrese su año de nacimiento: '))
+calculo = anio_actual - anio_nac
+if calculo >= 18:
+    print('Usted es mayor de edad.')
+else:
+    print('Usted es menor de edad.')
