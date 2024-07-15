c91739d390f3a786c6ae8def021ed5a9395c0d70
xlmriosx <xlmriosx@gmail.com>
2024-07-15 16:26:05 -0300

    clase 3 -- 2024-07-15

---
+++ estructuras-control-condicionales.py
@@ -0,0 +1,24 @@
+# condicionales
+# >, <, ==, <=, >=, !=
+maximo = 10
+minimo = 1
+## condicional simple
+# if (maximo < minimo): # operador de comparacion
+#     print("condicional simple: Cumplio la condicion")
+
+# ## condicional alternativo
+# if (maximo < minimo): # operador de comparacion
+#     print("condicional alternativo: Cumplio la condicion")
+# else:
+#     print("condicional alternativo: No cumplio la condicion")
+
+## condicional multiple
+opcion = 5
+if (opcion == 1):
+    print("Opcion: 1")
+elif (opcion == 2):
+    print("Opcion: 2")
+elif (opcion == 3):
+    print("Opcion: 3")
+else:
+    print("No se cumplio ninguna de las opciones")
