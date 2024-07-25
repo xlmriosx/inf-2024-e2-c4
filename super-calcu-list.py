68affd353db0e13be6b51f62453ac440db711df6
xlmriosx <xlmriosx@gmail.com>
2024-07-25 15:30:17 -0300

    solucion ejercicio

--- super-calcu-list.py
+++ super-calcu-list.py
@@ -18,28 +18,31 @@ condicion = 'si'
 
 print("Su lista cargada es la siguiente:", lista)
 
-print(f'''
-1 - Sumar todos los elementos de una lista
-2 - Encontrar el maximo
-3 - Encontrar el minimo
-4 - Filtrar por pares
-5 - Filtrar por impares
-''')
-opcion = int(input("Por favor ingrese una opcion: "))
+opcion = None
+while opcion != 0:
+    print(f'''
+    1 - Sumar todos los elementos de una lista
+    2 - Encontrar el maximo
+    3 - Encontrar el minimo
+    4 - Filtrar por pares
+    5 - Filtrar por impares
+    ''')
+    opcion = int(input("Por favor ingrese una opcion: "))
 
-if opcion == 1:
-    suma = sum(lista)
-    print(suma)
-elif opcion == 2:
-    maximo = max(lista)
-    print(maximo)
-elif opcion == 3:
-    minimo = min(lista)
-    print(minimo)
-elif opcion == 4:
-    filtradPar = list(filter(lambda x: x % 2 == 0, lista))
-    print(filtradPar)
-elif opcion == 5:
-    filtradoImpar = list(filter(lambda x: x % 2 != 0, lista))
-    print(filtradoImpar)
-
+    if opcion == 1:
+        suma = sum(lista)
+        print(suma)
+    elif opcion == 2:
+        maximo = max(lista)
+        print(maximo)
+    elif opcion == 3:
+        minimo = min(lista)
+        print(minimo)
+    elif opcion == 4:
+        filtradPar = list(filter(lambda x: x % 2 == 0, lista))
+        print(filtradPar)
+    elif opcion == 5:
+        filtradoImpar = list(filter(lambda x: x % 2 != 0, lista))
+        print(filtradoImpar)
+    else:
+        print('Su opcion ingresada no esta definida dentro de los limites de las opciones.')
